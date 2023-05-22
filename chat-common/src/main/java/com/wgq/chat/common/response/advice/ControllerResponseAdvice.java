ed157d17362803705a0480fd83f67c49536e9bd0
“wgq” <1351686755@qq.com>
2023-05-23 00:19:05 +0800

    基本完成注册和登录

--- chat-common/src/main/java/com/wgq/chat/common/response/advice/ControllerResponseAdvice.java
+++ chat-common/src/main/java/com/wgq/chat/common/response/advice/ControllerResponseAdvice.java
@@ -17,7 +17,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 public class ControllerResponseAdvice implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
-        //response是ResultVo类型，或者注释了NotControllerResponseAdvice都不进行包装
         return !methodParameter.getParameterType().isAssignableFrom(Result.class) ||
                 methodParameter.hasMethodAnnotation(NotControllerResponseAdvice.class);
     }
@@ -25,17 +24,14 @@ public class ControllerResponseAdvice implements ResponseBodyAdvice<Object> {
     @SneakyThrows
     @Override
     public Object beforeBodyWrite(Object data, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
-        //String类型不能直接包装
         if(methodParameter.getGenericParameterType().equals(String.class)){
             ObjectMapper objectMapper=  new ObjectMapper();
             try {
-                //将数据包装在ResultVo里后转换为json串进行返回
                 return objectMapper.writeValueAsString(Result.success(data));
             }catch (Exception e) {
                 throw new BusinessException(ResultCode.RESPONSE_PACK_ERROR,e.getMessage());
             }
         }
-        //否则直接包装成Result返回
         return Result.success(data);
     }
 }
