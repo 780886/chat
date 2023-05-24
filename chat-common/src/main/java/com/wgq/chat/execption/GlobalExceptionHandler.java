85141c44617759319da51b263e17954d91e8930e
wgq <1351686755@qq.com>
2023-05-24 16:19:46 +0800

    修改登录注册

--- chat-common/src/main/java/com/wgq/chat/execption/GlobalExceptionHandler.java
+++ chat-common/src/main/java/com/wgq/chat/execption/GlobalExceptionHandler.java
@@ -3,25 +3,33 @@ package com.wgq.chat.execption;
 
 import com.wgq.chat.common.enums.ResultCode;
 import com.wgq.chat.utils.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(BusinessException.class)
     public Result<Object> BusinessExceptionHandler(BusinessException e){
+        e.printStackTrace();
+        log.error("异常类型:{},业务信息:{}", e.getClass(),e.getMsg());
         return Result.fail(e.getCode(),e.getMsg());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public Result<Object> IllegalArgumentExceptionHandler(IllegalArgumentException e){
+        e.printStackTrace();
+        log.error("异常类型:{},业务信息:{}", e.getClass(),e.getMessage());
         return Result.fail(ResultCode.FAILED.getCode(),e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     public Result<Object> ExceptionHandler(Exception e){
+        e.printStackTrace();
+        log.error("异常类型:{},业务信息:{}", e.getClass(),e.getMessage());
         return Result.fail(ResultCode.FAILED);
     }
 }
