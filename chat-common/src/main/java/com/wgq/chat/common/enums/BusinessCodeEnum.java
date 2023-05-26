5beccde32a644fd5caf7b925665c445f965245cb
wgq <1351686755@qq.com>
2023-05-26 11:35:29 +0800

    修改登录认证

--- chat-common/src/main/java/com/wgq/chat/common/enums/BusinessCodeEnum.java
+++ chat-common/src/main/java/com/wgq/chat/common/enums/BusinessCodeEnum.java
@@ -13,9 +13,9 @@ public enum BusinessCodeEnum implements StatusCode {
     FAILED(500, "请求失败"),
     PARAM_NOT_EMPTY(1001, "参数不能为空，请重新输入!"),
     REQUEST_PARAM_INCOMPLETE(1002, "请求参数不完整，请重新输入!"),
-    RESPONSE_PACK_ERROR(1040, "response返回包装失败"),
-    TO_MANY_REQUEST(1060,"请求流量过大，请稍后再试"),
-    UN_KNOW_ERROR(1070,"系统未知异常"),
+    RESPONSE_PACK_ERROR(1040, "response返回包装失败!"),
+    TO_MANY_REQUEST(1060,"请求流量过大，请稍后再试!"),
+    UN_KNOW_ERROR(1080,"系统未知异常!"),
 //    验证码
     CAPTCHA_CODE_NOT_EMPTY(1101, "验证码不能为空,请重新输入验证码!"),
     CAPTCHA_CODE_NOT_EXIST(1102, "验证码不存在或已过期，请重新获取验证码!"),
@@ -26,11 +26,11 @@ public enum BusinessCodeEnum implements StatusCode {
     SMS_CODE_VALIDATE_ERROR(1112,"短信验证码验证失败,请重新输入!"),
     SMS_CODE_ERROR(1113, "验证码获取频率太高,稍后再试!"),
 //    手机号
-    PHONE_NOT_EMPTY(1201, "手机号不能为空,请重新输入"),
+    PHONE_NOT_EMPTY(1201, "手机号不能为空,请重新输入!"),
     PHONE_EXIST_ERROR(1202, "手机号已存在,,请重新输入!"),
     PHONE_FORMAT_ERROR(1203, "手机号格式不正确,请重新输入!"),
 //    邮箱
-    EMAIL_NOT_EMPTY(1301, "邮箱不能为空,请重新输入"),
+    EMAIL_NOT_EMPTY(1301, "邮箱不能为空,请重新输入!"),
     EMAIL_FORMAT_ERROR(1302, "邮箱格式不正确，请重新输入!"),
     EMAIL_EXIST_ERROR(1303, "邮箱已存在，请重新输入!"),
     //用户
@@ -47,6 +47,7 @@ public enum BusinessCodeEnum implements StatusCode {
 
     private int code;
     private String msg;
+
     BusinessCodeEnum(int code, String msg){
         this.code = code;
         this.msg = msg;
